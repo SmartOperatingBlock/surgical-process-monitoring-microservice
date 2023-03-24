ebfec5b9496b4690a9d8ecbb36d9b44e921c1bcd
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-03-24 11:27:04 +0100

    chore: add model data for room entity

---
+++ src/main/kotlin/entity/room/Room.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity.room
+
+/** The model of a Room of the Operating Block composed by:
+ * - the [id] of the room,
+ * - the [name] of the room,
+ * - the [type] of the room.
+ */
+data class Room(
+    val id: RoomData.RoomId,
+    val name: String?,
+    val type: RoomData.RoomType
+) {
+    override fun equals(other: Any?): Boolean = when {
+        other === this -> true
+        other is Room -> this.id == other.id
+        else -> false
+    }
+
+    override fun hashCode(): Int = this.id.hashCode()
+}
---
+++ src/main/kotlin/entity/room/RoomData.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity.room
+
+/** Module with all data necessary for Operating Block [Room]. */
+object RoomData {
+
+    /** The [id] of a [Room]. */
+    data class RoomId(val id: String) {
+        init {
+            require(this.id.isNotEmpty()) {
+                "Room ID can not be empty!"
+            }
+        }
+    }
+
+    /** The possible type of [Room]. */
+    enum class RoomType {
+        PRE_POST_OPERATING_ROOM, OPERATING_ROOM
+    }
+}
