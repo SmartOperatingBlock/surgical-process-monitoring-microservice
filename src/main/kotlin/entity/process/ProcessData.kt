1ec6bcfc50374440c022f63634250a881cf0a0ae
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-03-24 11:29:01 +0100

    chore: add process data

---
+++ src/main/kotlin/entity/process/ProcessData.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity.process
+
+/** Module with all data necessary for the [SurgicalProcess]. */
+object ProcessData {
+
+    /** The [id] of the [SurgicalProcess]. */
+    data class ProcessId(val id: String) {
+        init {
+            require(this.id.isNotEmpty()) {
+                "Process ID can not be empty!"
+            }
+        }
+    }
+
+    /** The different states of a [SurgicalProcess]. */
+    enum class ProcessState {
+        PRE_SURGERY, SURGERY, POST_SURGERY, INTERRUPTED
+    }
+
+    /** The different steps of a [SurgicalProcess]. */
+    enum class ProcessStep {
+        PATIENT_IN_PREPARATION, PATIENT_ON_OPERATING_TABLE, ANESTHESIA, SURGERY_IN_PROGRESS, END_OF_SURGERY,
+    }
+}
