b7862f2fcf79359bb5d4545bad77d12f01a510fa
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-03-27 12:58:22 +0200

    test(entity): add health professionals entity test

---
+++ src/test/kotlin/entity/healthprofessional/TestHealthProfessional.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity.healthprofessional
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class TestHealthProfessional : StringSpec({
+
+    "Health Professional Id should not be empty" {
+        shouldThrow<IllegalArgumentException> {
+            HealthProfessional(HealthProfessionalData.HealthProfessionalId(""))
+        }
+    }
+
+    "Health professional should be equal to another health professional with same id" {
+        val id = HealthProfessionalData.HealthProfessionalId("12345678")
+        HealthProfessional(id) shouldBe HealthProfessional(id)
+    }
+})
