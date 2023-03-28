c847648aadbe814cab115befda4f29e6b5022e01
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-03-28 21:27:00 +0200

    feat: create model of process event

---
+++ src/main/kotlin/application/presenter/event/model/ProcessEvent.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.presenter.event.model
+
+import application.presenter.event.model.payloads.ProcessEventsPayloads.ProcessEventPayload
+import kotlinx.serialization.Serializable
+
+/** The model of a Process Event composed by:
+ *  - the [key]
+ *  - the [data]
+ *  - the [dateTime].
+ */
+@Serializable
+data class ProcessEvent<E : ProcessEventPayload>(
+    override val key: String,
+    override val data: E,
+    override val dateTime: String
+) : Event<E>
+
+/** Module that wrap all the possible keys for process events. */
+object ProcessEventsKeys {
+    /** The key of the medical device usage event. */
+    const val MEDICAL_DEVICE_USAGE_EVENT = "MEDICAL_DEVICE_USAGE_EVENT"
+    /** The key of the medical technology usage event. */
+    const val MEDICAL_TECHNOLOGY_USAGE_EVENT = "MEDICAL_TECHNOLOGY_USAGE_EVENT"
+    /** The key of the patient on operating bed event. */
+    const val PATIENT_ON_OB_EVENT = "PATIENT_ON_OB_EVENT"
+    /** The key of the body temperature update event. */
+    const val PATIENT_BODY_TEMPERATURE_UPDATE_EVENT = "PATIENT_BODY_TEMPERATURE_UPDATE_EVENT"
+    /** The key of the diastolic pressure update event. */
+    const val PATIENT_DIASTOLIC_PRESSURE_UPDATE_EVENT = "PATIENT_DIASTOLIC_PRESSURE_UPDATE_EVENT"
+    /** The key of the systolic pressure update event. */
+    const val PATIENT_SYSTOLIC_PRESSURE_UPDATE_EVENT = "PATIENT_SYSTOLIC_PRESSURE_UPDATE_EVENT"
+    /** The key of the respiratory rate update event. */
+    const val PATIENT_RESPIRATORY_RATE_UPDATE_EVENT = "PATIENT_RESPIRATORY_RATE_UPDATE_EVENT"
+    /** The key of the saturation update event. */
+    const val PATIENT_SATURATION_UPDATE_EVENT = "PATIENT_SATURATION_UPDATE_EVENT"
+    /** The key of the heartbeat update event. */
+    const val PATIENT_HEARTBEAT_UPDATE_EVENT = "PATIENT_HEARTBEAT_UPDATE_EVENT"
+}
