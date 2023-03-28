77e1962e252766cc710922f0b20b276e1aaf7071
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-03-28 11:14:47 +0200

    chore: add medical device repository interface

---
+++ src/main/kotlin/usecase/repository/MedicalDeviceRepository.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package usecase.repository
+
+import entity.medicaldevice.MedicalDeviceData
+import entity.process.ProcessData
+import java.time.Instant
+
+/**
+ * Interface that models the repository to manage the medical device.
+ */
+interface MedicalDeviceRepository {
+
+    /**
+     * Add new usage of an implantable medical device.
+     * @return true if correctly added, false otherwise.
+     */
+    fun addMedicalDeviceUsage(
+        medicalDeviceId: MedicalDeviceData.ImplantableMedicalDeviceId,
+        processId: ProcessData.ProcessId
+    ): Boolean
+
+    /**
+     * Add new usage of a medical technology.
+     * @return true if correctly created, false otherwise.
+     */
+    fun addMedicalTechnologyUsage(
+        medicalTechnologyId: MedicalDeviceData.MedicalTechnologyId,
+        dateTime: Instant,
+        processId: ProcessData.ProcessId,
+        inUse: Boolean
+    ): Boolean
+}
