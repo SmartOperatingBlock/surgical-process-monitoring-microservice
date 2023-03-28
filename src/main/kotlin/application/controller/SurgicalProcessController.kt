2deba0ce11f21b3229232c8cad4bfdef45f5ac2b
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-03-28 21:28:17 +0200

    chore: implement surgical process controller

---
+++ src/main/kotlin/application/controller/SurgicalProcessController.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.controller
+
+import application.controller.manager.ProcessDatabaseManager
+import application.controller.manager.ProcessDigitalTwinManager
+import entity.process.ProcessData
+import entity.process.SurgicalProcess
+import usecase.repository.SurgicalProcessRepository
+
+/**
+ * The implementation of the [SurgicalProcessRepository].
+ */
+class SurgicalProcessController(
+    private val processDatabaseManager: ProcessDatabaseManager,
+    private val processDigitalTwinManager: ProcessDigitalTwinManager
+) : SurgicalProcessRepository {
+
+    override fun createSurgicalProcess(process: SurgicalProcess): SurgicalProcess? =
+        if (this.getSurgicalProcessById(process.id) != null) {
+            this.processDigitalTwinManager.createSurgicalProcess(process)
+            this.processDatabaseManager.createSurgicalProcess(process)
+        } else null
+
+    override fun getSurgicalProcessById(processId: ProcessData.ProcessId): SurgicalProcess? =
+        this.processDatabaseManager.getSurgicalProcessById(processId)
+
+    override fun getCurrentSurgicalProcesses(): Set<SurgicalProcess> =
+        this.processDatabaseManager.getCurrentSurgicalProcesses()
+}
