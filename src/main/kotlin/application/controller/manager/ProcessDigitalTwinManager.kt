468bacbde41ab8d4eb10d445652213ce9c3a637e
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-03-28 22:30:19 +0200

    chore: add method to update process state and step to digital twins manager

--- src/main/kotlin/application/controller/manager/ProcessDigitalTwinManager.kt
+++ src/main/kotlin/application/controller/manager/ProcessDigitalTwinManager.kt
@@ -8,6 +8,7 @@
 
 package application.controller.manager
 
+import entity.process.ProcessData
 import entity.process.SurgicalProcess
 
 /** This interface models the operation on Digital Twins of [SurgicalProcess]. */
@@ -17,4 +18,20 @@ interface ProcessDigitalTwinManager {
      * Create the digital twin of the surgical [process].
      */
     fun createSurgicalProcess(process: SurgicalProcess): Boolean
+
+    /**
+     * Update the [state] of a [SurgicalProcess] by its [processId].
+     */
+    fun updateSurgicalProcessState(
+        processId: ProcessData.ProcessId,
+        state: ProcessData.ProcessState
+    ): Boolean
+
+    /**
+     * Update the [step] of a [SurgicalProcess] by its [processId].
+     */
+    fun updateSurgicalProcessStep(
+        processId: ProcessData.ProcessId,
+        step: ProcessData.ProcessStep
+    ): Boolean
 }
