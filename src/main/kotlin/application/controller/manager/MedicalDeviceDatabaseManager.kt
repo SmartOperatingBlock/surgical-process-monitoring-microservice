8e49e0f772b121d189a141c9e852ce243ec43b2f
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-03-28 11:17:42 +0200

    chore: add patient medical data db manager interface

---
+++ src/main/kotlin/application/controller/manager/MedicalDeviceDatabaseManager.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.controller.manager
+
+import entity.medicaldevice.MedicalDeviceData
+import entity.process.ProcessData
+import java.time.Instant
+
+/**
+ * This interface models the manager of the database for medical devices.
+ */
+interface MedicalDeviceDatabaseManager {
+
+    /**
+     * Add new usage of an implantable medical device.
+     * @return true if correctly added, false otherwise.
+     */
+    fun addMedicalDeviceUsage(
+        medicalDeviceId: MedicalDeviceData.ImplantableMedicalDeviceId,
+        processId: ProcessData.ProcessId
+    ): Boolean
+
+    /**
+     * Add new usage of a medical technology.
+     * @return true if correctly created, false otherwise.
+     */
+    fun addMedicalTechnologyUsage(
+        medicalTechnologyId: MedicalDeviceData.MedicalTechnologyId,
+        dateTime: Instant,
+        processId: ProcessData.ProcessId,
+        inUse: Boolean
+    ): Boolean
+}
---
+++ src/main/kotlin/application/controller/manager/PatientMedicalDataDatabaseManager.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.controller.manager
+
+import entity.patient.PatientData
+import java.time.Instant
+
+/**
+ * This interface models the manager of the database for patient medical data.
+ */
+interface PatientMedicalDataDatabaseManager {
+
+    /**
+     * Update the [PatientData.MedicalData] of a patient.
+     * @return true if correctly updated, false otherwise.
+     */
+    fun updatePatientMedicalData(
+        patientId: PatientData.PatientId,
+        medicalData: PatientData.MedicalData,
+        dateTime: Instant
+    ): Boolean
+
+    /**
+     * Get the [PatientData.MedicalData] of a patient given the id and a range of time.
+     * @return a map with the instant as key and the medical data as value.
+     */
+    fun getPatientMedicalData(
+        patientId: PatientData.PatientId,
+        from: Instant,
+        to: Instant
+    ): Map<Instant, PatientData.MedicalData>
+
+    /**
+     * Get current patient medical data given the patient id.
+     * @return the current patient medical data.
+     */
+    fun getCurrentPatientMedicalData(
+        patientId: PatientData.PatientId
+    ): PatientData.MedicalData
+}
