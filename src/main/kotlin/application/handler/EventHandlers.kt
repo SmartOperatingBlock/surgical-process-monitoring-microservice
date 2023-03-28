5b4815866a49bbb6c3321a191088fa48ed91f5f1
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-03-28 22:28:56 +0200

    chore: add process info event handler

--- src/main/kotlin/application/handler/EventHandlers.kt
+++ src/main/kotlin/application/handler/EventHandlers.kt
@@ -12,9 +12,11 @@ import application.presenter.event.model.Event
 import application.presenter.event.model.ProcessEvent
 import application.presenter.event.model.payloads.ProcessEventsPayloads
 import application.service.MedicalDeviceServices
+import application.service.SurgicalProcessServices
 import entity.medicaldevice.MedicalDeviceData
 import entity.process.ProcessData
 import usecase.repository.MedicalDeviceRepository
+import usecase.repository.SurgicalProcessRepository
 import java.time.Instant
 
 /**
@@ -64,6 +66,28 @@ object EventHandlers {
                         medicalDeviceRepository
                     ).execute()
                 } else false
+            }
+        }
+    }
+
+    /**
+     * The handler for process information events.
+     */
+    class ProcessInfoEventHandler(
+        private val surgicalProcessRepository: SurgicalProcessRepository
+    ) : EventHandler {
+
+        override fun canHandle(event: Event<*>): Boolean = event.cast<ProcessEvent<*>> {
+            this.data.cast<ProcessEventsPayloads.ProcessInfo>()
+        }
+
+        override fun consume(event: Event<*>) {
+            event.cast<ProcessEvent<ProcessEventsPayloads.ProcessInfo>> {
+                SurgicalProcessServices.UpdateSurgicalProcessStep(
+                    ProcessData.ProcessId(this.data.processId),
+                    ProcessData.ProcessStep.PATIENT_ON_OPERATING_TABLE,
+                    surgicalProcessRepository
+                ).execute()
             }
         }
     }
