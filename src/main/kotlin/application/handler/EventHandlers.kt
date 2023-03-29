be9c4d5b3c15ca0799056436fd1ffebe24c28b67
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-03-29 11:26:32 +0200

    chore: add diastolic pressure update event handler

--- src/main/kotlin/application/handler/EventHandlers.kt
+++ src/main/kotlin/application/handler/EventHandlers.kt
@@ -12,10 +12,13 @@ import application.presenter.event.model.Event
 import application.presenter.event.model.ProcessEvent
 import application.presenter.event.model.payloads.ProcessEventsPayloads
 import application.service.MedicalDeviceServices
+import application.service.PatientDataServices
 import application.service.SurgicalProcessServices
 import entity.medicaldevice.MedicalDeviceData
+import entity.patient.PatientData
 import entity.process.ProcessData
 import usecase.repository.MedicalDeviceRepository
+import usecase.repository.PatientRepository
 import usecase.repository.SurgicalProcessRepository
 import java.time.Instant
 
@@ -73,20 +76,73 @@ object EventHandlers {
     /**
      * The handler for process information events.
      */
-    class ProcessInfoEventHandler(
+    class PatientOnOperatingTableEventHandler(
         private val surgicalProcessRepository: SurgicalProcessRepository
     ) : EventHandler {
 
         override fun canHandle(event: Event<*>): Boolean = event.cast<ProcessEvent<*>> {
-            this.data.cast<ProcessEventsPayloads.ProcessInfo>()
+            this.data.cast<ProcessEventsPayloads.PatientOnOperatingTable>()
         }
 
         override fun consume(event: Event<*>) {
-            event.cast<ProcessEvent<ProcessEventsPayloads.ProcessInfo>> {
-                SurgicalProcessServices.UpdateSurgicalProcessStep(
+            event.cast<ProcessEvent<ProcessEventsPayloads.PatientOnOperatingTable>> {
+                SurgicalProcessServices.UpdateSurgicalProcessState(
                     ProcessData.ProcessId(this.data.processId),
-                    ProcessData.ProcessStep.PATIENT_ON_OPERATING_TABLE,
+                    ProcessData.ProcessState.SURGERY,
                     surgicalProcessRepository
+                ).execute() &&
+                    SurgicalProcessServices.UpdateSurgicalProcessStep(
+                        ProcessData.ProcessId(this.data.processId),
+                        ProcessData.ProcessStep.PATIENT_ON_OPERATING_TABLE,
+                        surgicalProcessRepository
+                    ).execute()
+            }
+        }
+    }
+
+    /**
+     * The handler for Patient Body Temperature update events.
+     */
+    class BodyTemperatureUpdateEventHandler(
+        private val patientRepository: PatientRepository
+    ) : EventHandler {
+
+        override fun canHandle(event: Event<*>): Boolean = event.cast<ProcessEvent<*>> {
+            this.data.cast<ProcessEventsPayloads.PatientData<ProcessEventsPayloads.BodyTemperature>>()
+        }
+
+        override fun consume(event: Event<*>) {
+            event.cast<ProcessEvent<ProcessEventsPayloads.PatientData<ProcessEventsPayloads.BodyTemperature>>> {
+                PatientDataServices.UpdatePatientMedicalData(
+                    PatientData.PatientId(this.data.patientId),
+                    PatientData.MedicalData(bodyTemperature = PatientData.BodyTemperature(this.data.data.temperature)),
+                    Instant.parse(this.dateTime),
+                    patientRepository
+                ).execute()
+            }
+        }
+    }
+
+    /**
+     * The handler for Patient Body Temperature update events.
+     */
+    class DiastolicPressureUpdateEventHandler(
+        private val patientRepository: PatientRepository
+    ) : EventHandler {
+
+        override fun canHandle(event: Event<*>): Boolean = event.cast<ProcessEvent<*>> {
+            this.data.cast<ProcessEventsPayloads.PatientData<ProcessEventsPayloads.DiastolicPressure>>()
+        }
+
+        override fun consume(event: Event<*>) {
+            event.cast<ProcessEvent<ProcessEventsPayloads.PatientData<ProcessEventsPayloads.DiastolicPressure>>> {
+                PatientDataServices.UpdatePatientMedicalData(
+                    PatientData.PatientId(this.data.patientId),
+                    PatientData.MedicalData(
+                        diastolicBloodPressure = PatientData.DiastolicBloodPressure(this.data.data.pressure)
+                    ),
+                    Instant.parse(this.dateTime),
+                    patientRepository
                 ).execute()
             }
         }
