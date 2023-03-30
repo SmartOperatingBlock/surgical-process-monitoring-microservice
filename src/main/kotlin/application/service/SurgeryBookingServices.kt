67a9c47487f266486c6a93b6aa18fed68d8020d9
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-03-30 12:32:37 +0200

    chore: add surgery booking application service

---
+++ src/main/kotlin/application/service/SurgeryBookingServices.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.service
+
+import entity.booking.SurgeryBooking
+import entity.patient.PatientData
+import usecase.repository.BookingRepository
+
+/** Module with all application services of surgery booking. */
+object SurgeryBookingServices {
+
+    /**
+     * The [ApplicationService] to get a [SurgeryBooking] by a [patientId].
+     */
+    class GetSurgeryBookingByPatient(
+        private val patientId: PatientData.PatientId,
+        private val surgeryBookingRepository: BookingRepository
+    ) : ApplicationService<SurgeryBooking?> {
+        override fun execute(): SurgeryBooking? = surgeryBookingRepository.getSurgeryBookingByPatient(patientId)
+    }
+}
