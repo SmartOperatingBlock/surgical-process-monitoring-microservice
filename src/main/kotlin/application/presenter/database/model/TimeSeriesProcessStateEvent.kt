27239ca4be3052a75afddb4696a12739cf87530e
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-03-31 10:45:32 +0200

    chore: add time series process state event

---
+++ src/main/kotlin/application/presenter/database/model/TimeSeriesProcessStateEvent.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.presenter.database.model
+
+import entity.process.ProcessData
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
+import java.time.Instant
+
+/**
+ * Models a time series of a process state event.
+ * @param dateTime the datetime of the process state event.
+ * @param metadata metadata of the process state event.
+ * @param value the value of the process state event.
+ */
+@Serializable
+data class TimeSeriesProcessStateEvent(
+    @Contextual
+    val dateTime: Instant,
+    val metadata: TimeSeriesProcessStateEventMetadata,
+    val value: ProcessData.ProcessState
+)
+
+/**
+ * The metadata of a process state event.
+ * @param processId the id of the process.
+ */
+@Serializable
+data class TimeSeriesProcessStateEventMetadata(
+    val processId: ProcessData.ProcessId
+)
