2d13694a4afa94e97f49a0de58d8ea8713429a56
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-03-31 10:45:52 +0200

    chore: add time series process step event

---
+++ src/main/kotlin/application/presenter/database/model/TimeSeriesProcessStepEvent.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.presenter.database.model
+
+import entity.process.ProcessData
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
+import java.time.Instant
+
+/**
+ * Models a time series of a process step event.
+ * @param dateTime the datetime of the process step event.
+ * @param metadata metadata of the process step event.
+ * @param value the value of the process step event.
+ */
+@Serializable
+data class TimeSeriesProcessStepEvent(
+    @Contextual
+    val dateTime: Instant,
+    val metadata: TimeSeriesProcessStepEventMetadata,
+    val value: ProcessData.ProcessStep
+)
+
+/**
+ * The metadata of a process step event.
+ * @param processId the id of the process.
+ */
+@Serializable
+data class TimeSeriesProcessStepEventMetadata(
+    val processId: ProcessData.ProcessId
+)
