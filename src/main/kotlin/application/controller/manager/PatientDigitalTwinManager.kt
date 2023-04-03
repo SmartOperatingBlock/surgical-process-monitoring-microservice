aeb499fe9dd2e3030b04f23951c5a6e6e8c73194
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-04-03 11:54:51 +0200

    chore: create patient digital twin manager

---
+++ src/main/kotlin/application/controller/manager/PatientDigitalTwinManager.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.controller.manager
+
+import entity.patient.Patient
+import entity.patient.PatientData
+
+/** This interface model the operation on Patients Digital Twins. */
+interface PatientDigitalTwinManager {
+
+    /**
+     * Create the digital twin of a [Patient] given its [patientId].
+     */
+    fun createPatientDT(
+        patientId: PatientData.PatientId
+    ): Patient?
+}
