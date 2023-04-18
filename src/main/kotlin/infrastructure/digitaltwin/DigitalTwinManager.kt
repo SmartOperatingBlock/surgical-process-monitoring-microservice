9fab7537cc38dea2f4621f0b38c667101ebb7ab9
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-04-18 15:01:45 +0200

    chore: implement azure digital twin operation

--- src/main/kotlin/infrastructure/digitaltwin/DigitalTwinManager.kt
+++ src/main/kotlin/infrastructure/digitaltwin/DigitalTwinManager.kt
@@ -12,6 +12,7 @@ import application.controller.manager.MedicalDeviceDigitalTwinManager
 import application.controller.manager.PatientDigitalTwinManager
 import application.controller.manager.ProcessDigitalTwinManager
 import application.controller.manager.SurgeryBookingDigitalTwinManager
+import com.azure.core.models.JsonPatchDocument
 import com.azure.digitaltwins.core.BasicDigitalTwin
 import com.azure.digitaltwins.core.BasicRelationship
 import com.azure.digitaltwins.core.DigitalTwinsClient
@@ -25,7 +26,17 @@ import entity.patient.Patient
 import entity.patient.PatientData
 import entity.process.ProcessData
 import entity.process.SurgicalProcess
+import entity.room.RoomData
+import infrastructure.digitaltwin.presenter.PatientAdt.toDigitalTwin
+import infrastructure.digitaltwin.presenter.SurgeryBookingAdt
 import infrastructure.digitaltwin.presenter.SurgeryBookingAdt.toSurgeryBooking
+import infrastructure.digitaltwin.presenter.SurgicalProcessAdt
+import infrastructure.digitaltwin.presenter.SurgicalProcessAdt.toDigitalTwin
+import infrastructure.digitaltwin.query.AdtQuery
+import infrastructure.digitaltwin.query.AdtQuery.Companion.AdtQueryUtils.eq
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 
 /** The manager of Digital Twins Operations. */
 class DigitalTwinManager :
@@ -48,40 +59,138 @@ class DigitalTwinManager :
 
     override fun findSurgicalProcessByMedicalTechnology(
         medicalTechnologyId: MedicalDeviceData.MedicalTechnologyId
-    ): SurgicalProcess? {
-        TODO("Not yet implemented")
-    }
+    ): ProcessData.ProcessId? =
+        this.dtClient.applySafeDigitalTwinOperation(null) {
+            val roomId = query(
+                AdtQuery
+                    .createQuery()
+                    .selectTop(1, "CT.\$dtId")
+                    .fromDigitalTwins("T")
+                    .joinRelationship(
+                        "CT",
+                        "T",
+                        "rel_is_located",
+                    ).where("T.\$dtId" eq medicalTechnologyId.id).query,
+                String::class.java,
+            ).let {
+                if (it.count() == 1) {
+                    Json.parseToJsonElement(it.first()).jsonObject["\$dtId"]?.let { id ->
+                        RoomData.RoomId(id.jsonPrimitive.content)
+                    }
+                } else {
+                    null
+                }
+            }
+            roomId?.let {
+                query(
+                    AdtQuery.createQuery()
+                        .selectTop(1, "Process.\$dtId")
+                        .fromDigitalTwins("Process")
+                        .joinRelationship("Room", "Process", SurgicalProcessAdt.ROOM_RELATIONSHIP)
+                        .where("Room.\$dtId" eq it.id).query,
+                    String::class.java,
+                ).let { result ->
+                    if (result.count() == 1) {
+                        Json.parseToJsonElement(result.first()).jsonObject["\$dtId"]?.let { id ->
+                            ProcessData.ProcessId(id.jsonPrimitive.content)
+                        }
+                    } else {
+                        null
+                    }
+                }
+            }
+        }
 
-    override fun createPatientDT(patientId: PatientData.PatientId): Patient? {
-        TODO("Not yet implemented")
-    }
+    override fun createPatientDT(patientId: PatientData.PatientId): Patient? =
+        with(Patient(patientId, medicalData = PatientData.MedicalData()).toDigitalTwin()) {
+            dtClient.applySafeDigitalTwinOperation(null) {
+                createOrReplaceDigitalTwin(this@with.id, this@with, BasicDigitalTwin::class.java)
+                Patient(patientId, medicalData = PatientData.MedicalData())
+            }
+        }
 
-    override fun createSurgicalProcess(process: SurgicalProcess): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun createSurgicalProcess(process: SurgicalProcess): Boolean =
+        with(process.toDigitalTwin()) {
+            dtClient.applySafeDigitalTwinOperation(false) {
+                createOrReplaceDigitalTwin(this@with.id, this@with, BasicDigitalTwin::class.java)
+                val booking = getSurgeryBookingByPatient(process.patientId)
+                booking?.let {
+                    createOrReplaceRelationship(
+                        process.id.id,
+                        "${process.id.id}-${it.id.id}",
+                        BasicRelationship(
+                            "${process.id.id}-${it.id.id}",
+                            process.id.id,
+                            it.id.id,
+                            SurgicalProcessAdt.BOOKING_RELATIONSHIP
+                        ),
+                        BasicRelationship::class.java
+                    )
+                    createOrReplaceRelationship(
+                        process.id.id,
+                        "${process.id.id}-${it.patientId.id}",
+                        BasicRelationship(
+                            "${process.id.id}-${it.patientId.id}",
+                            process.id.id,
+                            it.patientId.id,
+                            SurgicalProcessAdt.PATIENT_RELATIONSHIP
+                        ),
+                        BasicRelationship::class.java
+                    )
+                }
+                true
+            }
+        }
 
     override fun updateSurgicalProcessState(
         processId: ProcessData.ProcessId,
         state: ProcessData.ProcessState
-    ): Boolean {
-        TODO("Not yet implemented")
-    }
+    ): Boolean =
+        this.dtClient.applySafeDigitalTwinOperation(false) {
+            updateDigitalTwin(processId.id, JsonPatchDocument().appendAdd("process_state", state.ordinal))
+            true
+        }
 
-    override fun updateSurgicalProcessStep(processId: ProcessData.ProcessId, step: ProcessData.ProcessStep): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun updateSurgicalProcessStep(processId: ProcessData.ProcessId, step: ProcessData.ProcessStep): Boolean =
+        this.dtClient.applySafeDigitalTwinOperation(false) {
+            updateDigitalTwin(processId.id, JsonPatchDocument().appendAdd("process_step", step.ordinal))
+            true
+        }
 
     override fun getSurgeryBookingByPatient(patientId: PatientData.PatientId): SurgeryBooking? =
         this.dtClient.applySafeDigitalTwinOperation(null) {
-            getDigitalTwin(patientId.id, BasicDigitalTwin::class.java).mapRelationships().toSurgeryBooking()
+            val bookingId: String? = query(
+                AdtQuery.createQuery()
+                    .selectTop(1, "T.\$dtId")
+                    .fromDigitalTwins("T")
+                    .joinRelationship("CT", "T", SurgeryBookingAdt.PATIENT_RELATIONSHIP)
+                    .where("CT.\$dtId" eq patientId.id).query,
+                String::class.java
+            ).run {
+                if (this.count() == 1) {
+                    Json.parseToJsonElement(this.first()).jsonObject["\$dtId"]?.jsonPrimitive?.content
+                } else {
+                    null
+                }
+            }
+            bookingId?.let {
+                getDigitalTwin(bookingId, BasicDigitalTwin::class.java).toSurgeryBooking()
+            }
         }
 
     override fun removePatientSurgeryBookingMapping(
         patientId: PatientData.PatientId,
         surgeryBookingId: SurgeryBookingData.SurgeryBookingId
-    ): Boolean {
-        TODO("Not yet implemented")
-    }
+    ): Boolean =
+        this.dtClient.applySafeDigitalTwinOperation(false) {
+            this.listRelationships(surgeryBookingId.id, BasicRelationship::class.java).firstOrNull {
+                it.targetId == patientId.id
+            }?.let {
+                this.deleteRelationship(surgeryBookingId.id, it.id)
+                true
+            }
+            false
+        }
 
     private fun <R> DigitalTwinsClient.applySafeDigitalTwinOperation(
         defaultResult: R,
@@ -101,7 +210,8 @@ class DigitalTwinManager :
         private const val dtEndpointVariable = "AZURE_DT_ENDPOINT"
     }
 
-    private fun BasicDigitalTwin.mapRelationships(): BasicDigitalTwin {
+    /** Utility method to insert target relationships to the map with digital twins properties. */
+    fun BasicDigitalTwin.mapRelationships(): BasicDigitalTwin {
         dtClient.listRelationships(this.id, BasicRelationship::class.java).forEach {
             this.contents[it.name] = it.targetId
         }
