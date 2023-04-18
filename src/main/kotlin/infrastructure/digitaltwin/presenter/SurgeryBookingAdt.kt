653577f1f44dfc2e14874abad17b31239a20ad56
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-04-18 15:01:16 +0200

    docs: add some documentation for adt relationships

--- src/main/kotlin/infrastructure/digitaltwin/presenter/SurgeryBookingAdt.kt
+++ src/main/kotlin/infrastructure/digitaltwin/presenter/SurgeryBookingAdt.kt
@@ -24,7 +24,8 @@ object SurgeryBookingAdt {
     private const val DATETIME_PROPERTY = "booking_date_time"
     private const val TYPE_PROPERTY = "surgery_type"
 
-    private const val PATIENT_RELATIONSHIP = "rel_booking_associated_patient"
+    /** The relationship between patient and booking dt. */
+    const val PATIENT_RELATIONSHIP = "rel_booking_associated_patient"
     private const val RESPONSIBLE_HEALTH_PROFESSIONAL_RELATIONSHIP = "rel_responsible_health_professional"
 
     /** Extension method to convert a [BasicDigitalTwin] to a [SurgeryBooking]. */
