ff0b6a706aa6a0a402b60d4f6e77ee9ba277a485
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-04-27 14:50:18 +0200

    feat: create event producer interface

---
+++ src/main/kotlin/application/controller/manager/EventProducer.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.controller.manager
+
+import application.presenter.event.model.Event
+
+/**
+ * The [Event] producer.
+ */
+interface EventProducer {
+
+    /** Produce the [event] into an event broker. */
+    fun produceEvent(event: Event<*>)
+}
