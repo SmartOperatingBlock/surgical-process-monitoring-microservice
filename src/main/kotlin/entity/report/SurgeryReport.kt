9e2da7144c888e23ce6390df712471fe1c48632f
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-04-27 13:46:38 +0200

    feat: add surgery report entity

---
+++ src/main/kotlin/entity/report/SurgeryReport.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity.report
+
+import entity.healthprofessional.HealthProfessionalData
+import entity.patient.PatientData
+import entity.process.ProcessData
+import entity.room.Room
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
+import java.time.Instant
+
+/**
+ * The Report of the Surgical Process.
+ * It includes:
+ * - the [processId]
+ * - the [processType]
+ * - the [patientId]
+ * - the [healthProfessionalId]
+ * - the [preOperatingRoom]
+ * - the [operatingRoom]
+ * - the [processStates]
+ * - the [processSteps]
+ */
+@Serializable
+data class SurgeryReport(
+    val processId: ProcessData.ProcessId,
+    val processType: String,
+    val patientId: PatientData.PatientId,
+    val healthProfessionalId: HealthProfessionalData.HealthProfessionalId,
+    val preOperatingRoom: Room,
+    val operatingRoom: Room,
+    val processStates: List<Pair<@Contextual Instant, ProcessData.ProcessState>>,
+    val processSteps: List<Pair<@Contextual Instant, ProcessData.ProcessStep>>
+)
